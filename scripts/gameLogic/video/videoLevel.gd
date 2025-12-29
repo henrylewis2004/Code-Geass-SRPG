846de5ab60ee21f7e838d811be87d6d6ec5a1da5
henrylewis2004 <henry@lewis-lewis.co.uk>
2025-12-29 11:22:11 +0000

    chapters

--- scripts/gameLogic/video/videoLevel.gd
+++ scripts/gameLogic/video/videoLevel.gd
@@ -1,6 +1,28 @@
 class_name VideoLevel extends Level
 
-func _process(delta):
-    if Input.is_action_just_pressed("menu_start"):
-        sceneOver.emit()
+@export var timeSkip: float = 2.0
 
+@onready var timer: Timer = $Timer
+var skip: bool = false
+
+func startSkip() -> void:
+	skip = true
+	timer.start(timeSkip)
+
+	await timer.timeout
+	if skip:
+		sceneOver.emit()
+
+
+func stopSkip() -> void:
+	skip = false
+	timer.stop()
+
+
+####
+func _process(delta):
+	if Input.is_action_just_pressed("menu_start"):
+		startSkip()
+
+	elif Input.is_action_just_released("menu_start"):
+		stopSkip()
