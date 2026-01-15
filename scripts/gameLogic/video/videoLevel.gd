365623e5ab48660a92945f85aee5e309023bd365
henrylewis2004 <henry@lewis-lewis.co.uk>
2026-01-15 21:20:16 +0000

    skip anim

--- scripts/gameLogic/video/videoLevel.gd
+++ scripts/gameLogic/video/videoLevel.gd
@@ -3,19 +3,26 @@ class_name VideoLevel extends Level
 @export var timeSkip: float = 2.0
 
 @onready var timer: Timer = $Timer
+@onready var skipAnimPlayer: AnimationPlayer = $SkipAnimation.get_node("AnimationPlayer")
 var skip: bool = false
 
 func startSkip() -> void:
 	skip = true
 	timer.start(timeSkip)
+	skipAnimPlayer.play("skip")
 
 	await timer.timeout
 	if skip:
+		set_process(false)
+		skipAnimPlayer.play("endSkip")
+		await skipAnimPlayer.animation_finished
 		sceneOver.emit()
+
 
 
 func stopSkip() -> void:
 	skip = false
+	skipAnimPlayer.play("RESET")
 	timer.stop()
 
 
